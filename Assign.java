e78b77b7da8434570a866b869568fb77482e9feb
NitishTadikonda <nitishtadikonda@gmail.com>
2017-10-11 21:22:45 +0530

    Updated

--- Assign.java
+++ Assign.java
@@ -1,16 +1,25 @@
-/* 
+/*
 1 1 2 2 3 3 
 1 2 2 3 3 4 
 2 2 3 3 4 4 
 2 3 3 4 4 5 
-
+3 3 4 4 5 5
  */
+import java.util.Scanner;
 class Test
 {
 	public static void main(String[] args)
 	{
-		int c=0;int b=1;int d=0;int a=1;int x=2;int y=1;
-	for(int i=1;i<=4;i++)
+		int c=2;
+		int b=1;
+		int d=2;
+		int a=1;
+		int x=1;
+		int y=2;
+		Scanner sc = new Scanner (System.in);
+		System.out.println("Enter number of rows:");
+		int n = sc.nextInt();
+	for(int i=1;i<=n;i++)
 	{
 		
 		for(int j=1;j<=3;j++)
@@ -23,15 +32,17 @@ class Test
 		}
 		if(i % 2 == 1)
 		{
-			a=x-i;
-			b=y+1;
-			x+=3;
+			a=x;
+			b=y;
+			x++;
 			y++;
 		}
 		else
 		{
-			a = c+i;
-			b=d+i;
+			a =c;
+			b=d;
+			c++;
+			d++;
 		}
 		System.out.println();
 	}
